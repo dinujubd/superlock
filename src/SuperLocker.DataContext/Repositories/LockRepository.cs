3143bda4b57beda9d55baadf94eaf378a6ee5284
dinujubd <maildinuju@gmail.com>
2021-07-06 23:48:10 +0700

    command with dapper done

--- src/SuperLocker.DataContext/Repositories/LockRepository.cs
+++ src/SuperLocker.DataContext/Repositories/LockRepository.cs
@@ -1,21 +1,70 @@
+using System;
 using System.Threading.Tasks;
+using Dapper;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 using SuperLocker.Core.Command;
 using SuperLocker.Core.Repositories;
 
-namespace SuperLocker.DataContext.Repositories {
+namespace SuperLocker.DataContext.Repositories
+{
     public class LockRepository : ILockRepository
     {
         private readonly ILogger<LockCommand> _logger;
+        private readonly MySqlConnection _conn;
+
+        private string _connectionString = "Server=localhost;Database=AppDBSuperLock;Uid=root;Pwd=rpass;";
 
         public LockRepository(ILogger<LockCommand> logger)
         {
             _logger = logger;
+            _conn = new MySqlConnection(_connectionString);
         }
-        public Task Lock(LockCommand lockInfo)
+        public async Task Lock(LockCommand lockInfo)
         {
-            _logger.LogInformation("coming to papa");
-            return Task.CompletedTask;
+            var sql = "SELECT * from AppDBSuperLock.user_locks where lock_id = @LockId and user_id = @UserId";
+
+            var result = await this._conn.QueryFirstOrDefaultAsync<UserLock>(sql, new { LockId = lockInfo.LockId.ToString(), UserId = lockInfo.UserId.ToString() });
+
+            if (result == null)
+            {
+                throw new InvalidOperationException("Userd lock has no match");
+            }
+
+            await ApplyUnlock(lockInfo);
+        }
+
+        private async Task ApplyUnlock(LockCommand lockInfo)
+        {
+            var unlockQuery = @"INSERT into AppDBSuperLock.unlock_activity_logs (unlock_activity_id, lock_id, user_id, unlock_time)
+                                values (@Id, @LockId, @UserId, @UnlockTime);
+                                ";
+            await this._conn.ExecuteAsync(unlockQuery, new
+            {
+                Id = Guid.NewGuid().ToString(),
+                LockId = lockInfo.LockId.ToString(),
+                UserId = lockInfo.UserId.ToString(),
+                UnlockTime = DateTime.UtcNow
+            });
         }
     }
+
+    public class UserLock
+    {
+        public Guid Id
+        {
+            get
+            {
+                return new Guid(user_lock_id);
+            }
+            private set
+            {
+                Id = value;
+            }
+        }
+        public string user_lock_id { get; set; }
+        public string lock_id { get; set; }
+        public string user_id { get; set; }
+    }
+
 }
