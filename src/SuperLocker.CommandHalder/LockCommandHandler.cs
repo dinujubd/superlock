9b95e2e4bdbdcb661063989dcaa145eddcced63f
Mohammad Mahmudul Hasan <maildinuju@gmail.com>
2021-07-08 18:25:51 +0700

    wrapped with validator

--- src/SuperLocker.CommandHalder/LockCommandHandler.cs
+++ src/SuperLocker.CommandHalder/LockCommandHandler.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using MassTransit;
 using SuperLocker.Core;
 using SuperLocker.Core.Command;
@@ -8,15 +9,21 @@ namespace SuperLocker.CommandHandler
 {
     public class UnlockCommandHandler : ICommandHandler<UnlockCommand>
     {
-
         private readonly ILockRepository _lockRepository;
-        public UnlockCommandHandler(ILockRepository lockRepository)
+        private readonly IValidator<UnlockCommand> _valdator;
+        public UnlockCommandHandler(ILockRepository lockRepository, IValidator<UnlockCommand> valdator)
         {
+            _valdator = valdator;
             _lockRepository = lockRepository;
         }
         public async Task Consume(ConsumeContext<UnlockCommand> context)
         {
-            await _lockRepository.Unlock(context.Message);
+            var validationResult = await _valdator.ValidateAsync(context.Message);
+            
+            if (validationResult.IsValid)
+            {
+                await _lockRepository.Unlock(context.Message);
+            }
         }
     }
 }
