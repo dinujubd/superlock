59a5b762ce629c8baf954d643d8bd3d4f5ff773e
Mohammad Hasan <mohammad.hasan@agoda.com>
2021-07-09 17:24:26 +0700

    mysql config from appsettings

--- src/SuperLocker.DataContext/Providers/MySqlConnectionProvider.cs
+++ src/SuperLocker.DataContext/Providers/MySqlConnectionProvider.cs
@@ -1,14 +1,20 @@
+using Microsoft.Extensions.Options;
 using MySql.Data.MySqlClient;
+using SuperLocker.Crosscuts;
 
 namespace SuperLocker.DataContext.Providers
 {
     public class MySqlConnectionProvider : IDatabaseConnectionProvider<MySqlConnection>
     {
-        private readonly string _connectionString = "Server=db;Database=AppDBSuperLock;Uid=root;Pwd=rpass;";
+        private readonly IOptions<DatabaseConfigurations> _config;
 
+        public MySqlConnectionProvider(IOptions<DatabaseConfigurations> config)
+        {
+            _config = config;
+        }
         public MySqlConnection Get()
         {
-            return new MySqlConnection(_connectionString);
+            return new MySqlConnection(_config.Value.ConnectionString);
         }
     }
 }
