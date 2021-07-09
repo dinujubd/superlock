fcc2f6ef3a814c6801e40b5b2d80d6ce84ec3f6c
Mohammad Mahmudul Hasan <maildinuju@gmail.com>
2021-07-09 22:08:11 +0700

    Controllers separated

---
+++ src/SuperLocker.Api/Controllers/UnlockCommandController.cs
@@ -0,0 +1,40 @@
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SuperLocker.Api.Models;
+using SuperLocker.Core.Command;
+using System.Threading.Tasks;
+
+namespace SuperLocker.Api.Controllers
+{
+    [Authorize(Roles = "admin,appuser")]
+    [ApiController]
+    [Route("[Controller]/[Action]")]
+    public class UnlockCommandController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private readonly AppUser _user;
+
+        public UnlockCommandController(IBus bus, AppUser user)
+        {
+            _bus = bus;
+            _user = user;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Unlock(UnlockRequest request)
+        {
+            try
+            {
+                await _bus.Publish(new UnlockCommand(request.LockId, _user.UserId));
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }   
+        }
+
+    }
+
+}
