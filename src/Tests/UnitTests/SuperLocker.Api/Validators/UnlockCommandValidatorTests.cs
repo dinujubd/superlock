3d00381b0a9c92bfcfef2c6947b503657d5e7225
Mohammad Hasan <mohammad.hasan@agoda.com>
2021-07-09 14:49:51 +0700

    tests adjusted

--- src/Tests/UnitTests/SuperLocker.Api/Validators/UnlockCommandValidatorTests.cs
+++ src/Tests/UnitTests/SuperLocker.Api/Validators/UnlockCommandValidatorTests.cs
@@ -8,19 +8,18 @@ namespace SuperLocker.Api.Tests.Validators
     public class UnlockRequestValidatorTests
     {
         [Theory]
-        [InlineData("00000000-0000-0000-0000-000000000000", "71d5352c-dfb7-11eb-ba80-0242ac130004", false, "UserId")]
-        [InlineData("71d5352c-dfb7-11eb-ba80-0242ac130004", "00000000-0000-0000-0000-000000000000",  false, "LockId")]
-        [InlineData("71d5352c-dfb7-11eb-ba80-0242ac130004", "d1d6f8e8-dfb7-11eb-ba80-0242ac130004", true, "")]
-        public void UnlockCommandShouldNotAcceptAnyEmptyUserOrLock(Guid userId, Guid lockId, bool isValid, string invalidProperyName)
+        [InlineData("00000000-0000-0000-0000-000000000000", false, "UserId")]
+        [InlineData("71d5352c-dfb7-11eb-ba80-0242ac130004", true, "")]
+        public void UnlockCommandShouldNotAcceptAnyEmptyUserOrLock(Guid lockId, bool isValid, string invalidProperyName)
         {
             var unlockValidator = new UnlockRequestValidator();
-            var command = new UnlockRequest { LockId = lockId, UserId = userId };
+            var command = new UnlockRequest { LockId = lockId };
 
             var validationResult = unlockValidator.Validate(command);
 
             Assert.Equal(isValid, validationResult.IsValid);
-            if(!isValid)
-            Assert.Contains(validationResult.Errors, o => o.PropertyName == invalidProperyName);
+            if (!isValid)
+                Assert.Contains(validationResult.Errors, o => o.PropertyName == invalidProperyName);
         }
     }
 }
